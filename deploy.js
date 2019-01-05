7a2f59b223693ab964831ae8601a50edcd5eb572
frederickalcantara <frederickaalcantara@gmail.com>
2019-01-04 22:34:01 -0500

    Added deploy file for smart contracts

--- deploy.js
+++ deploy.js
@@ -1,21 +1,18 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+const { mnemonic, APIRoute } = require('./env/truflle-wallet');
 
-const provider = new HDWalletProvider(
-  'call tide couple urge chuckle abstract page bundle retreat fever pool airport',
-  'https://rinkeby.infura.io/v3/1fcf214658b94f38a84e928bff2fb4f9'
-);
+const provider = new HDWalletProvider(mnemonic, APIRoute);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
   console.log(`Attempting to deploy from account: ${accounts[0]}`);
-
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello World!'] })
-    .send({ from: accounts[0], gas: '1000000' });
+    .deploy({ data: `0x${bytecode}`, arguments: ['Hello World!'] })
+    .send({ from: accounts[0], gas: 1000000 });
 
   console.log(`Contracted deployed to ${result.options.address}`);
 };
